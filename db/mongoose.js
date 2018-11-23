9d3569869652e72538a508e6367db061162ff4fc
LukeyBeachBoy <lb580@kent.ac.uk>
2018-11-23 15:34:24 +0000

    Corrected MONGODB_URI var

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/myApp', {useNewUrlParser : true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/myApp', {useNewUrlParser : true});
 
 exports.mongoose = mongoose;
