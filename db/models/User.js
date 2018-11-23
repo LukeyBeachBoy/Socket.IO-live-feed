36cd5b19e15516d3b2df159106b8c36f3d7bb8c3
LukeyBeachBoy <lb580@kent.ac.uk>
2018-11-23 15:22:30 +0000

    First commit

---
+++ db/models/User.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+    name : String,
+    age : Number
+});
+module.exports = {User};
---
+++ server.js
@@ -0,0 +1,43 @@
+const port = process.env.PORT || 3000;
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const hbs = require('hbs');
+
+const {mongoose} = require('./db/mongoose');
+const {User} = require('./db/models/User');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.set('view-engine', hbs);
+hbs.registerPartials('../views/home.hbs');
+
+app.get('/', (req, res) => {
+    User.find((err, result) => {
+        if (err) {
+            return res.status(404).send(err);
+        }
+        res.status(200).render('../views/home.hbs', {allUsers : result});
+    });  
+});
+
+app.post('/users/', (req, res) => {
+    console.log(req);
+    var user = req.body;
+    if (user.name === undefined || user.age === undefined){
+        res.status(400).send('No user was submitted');
+    }
+    var newUser = new User(user);
+    newUser.save().then((user) => {
+        res.send(user);
+    }).catch((err) => {
+        res.send(err);
+    });
+});
+
+
+app.listen(port);
+console.log(`Server running on port: ${port}`);
