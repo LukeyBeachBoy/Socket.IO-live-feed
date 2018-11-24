e505c31b2bf0a7095a613a5e482924ba0de687a3
LukeyBeachBoy <lb580@kent.ac.uk>
2018-11-24 16:09:24 +0000

    Added azure connection string

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const address = process.env.MONGODB_URI || 'mongodb://localhost:27017/myApp';
+const address = process.env.MONGODB_URI || process.env.CUSTOMCONNSTR_MyConnectionString || 'mongodb://localhost:27017/myApp';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(address, {useNewUrlParser : true});
